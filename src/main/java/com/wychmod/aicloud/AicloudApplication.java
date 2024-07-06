ed8de2a8138edbc4b5ba10b7db538be3561602fa
wychmod <545480453@qq.com>
2024-07-06 09:51:34 +0800

    项目初始化

---
+++ src/main/java/com/wychmod/aicloud/AicloudApplication.java
@@ -0,0 +1,13 @@
+package com.wychmod.aicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wychmod/aicloud/AicloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wychmod.aicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
