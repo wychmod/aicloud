eabdefee029acb8d97302e49ae4729cfd75593e5
wychmod <545480453@qq.com>
2024-07-11 01:00:38 +0800

    feat(captcha): ✨ 完成验证码的生成接口

---
+++ src/main/java/com/wychmod/aicloud/controller/CaptchaController.java
@@ -0,0 +1,45 @@
+package com.wychmod.aicloud.controller;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.crypto.SecureUtil;
+import com.wychmod.aicloud.util.MinIoUtil;
+import com.wychmod.aicloud.util.ResponseEntity;
+import io.minio.errors.*;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+
+@Controller
+@RequestMapping("/captcha")
+public class CaptchaController {
+
+    @Resource
+    private MinIoUtil minIoUtil;
+
+    /**
+     * 生成验证码
+     */
+    @RequestMapping("/create")
+    public ResponseEntity createCaptcha(HttpServletRequest request) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        String url = "";
+        // 定义图形验证码的长和宽
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(120, 40);
+        String fileName = "captcha-" + SecureUtil.md5(request.getRemoteAddr());
+        try (InputStream inputStream = new ByteArrayInputStream(lineCaptcha.getImageBytes())) {
+            url = minIoUtil.upload(fileName, inputStream, "image/png");
+            // String code = lineCaptcha.getCode(); // 正确的验证码
+            // redisTemplate.opsForValue().set(fileName, code,60, TimeUnit.SECONDS); // 验证码存储到 Redis
+        }
+        return ResponseEntity.success(url);
+    }
+
+}
